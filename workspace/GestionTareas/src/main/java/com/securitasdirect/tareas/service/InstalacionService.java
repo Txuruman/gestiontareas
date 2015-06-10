39154e0744075edeac32189d8ec4724b7362156e
javiernaval <javiernaval@gmail.com>
2015-06-10 14:05:00 +0200

    initial

---
+++ workspace/GestionTareas/src/main/java/com/securitasdirect/tareas/service/InstalacionService.java
@@ -0,0 +1,7 @@
+package com.securitasdirect.tareas.service;
+
+/**
+ * Created by Javier Naval on 10/06/2015.
+ */
+public class InstalacionService {
+}
---
+++ workspace/GestionTareas/src/test/java/com/securitasdirect/tareas/client/InstallationMonDataService.java
@@ -0,0 +1,12 @@
+package com.securitasdirect.tareas.client;
+
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Test del Servicio de InstallationMonDataService
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:spring/applicationContext-test.xml" })
+@Transactional
+public class InstallationMonDataService {
+}
