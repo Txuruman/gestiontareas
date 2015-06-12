b6a01d7dc8a9a954323791f73dd979c8a97ee0a5
jeltv <jesus-angel.luque@teamvision.es>
2015-06-12 09:54:41 +0200

    iuouo

---
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/buscartarea.java
@@ -0,0 +1,23 @@
+package es.securitasdirect.tareas.web.controller;
+
+import vistasController.pestañas.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class buscartarea implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
+        ModelAndView mv = new ModelAndView("buscartarea");
+        String tecla1 = "active";
+        String tecla2 = "inactive";
+        String tecla3 = "inactive";
+        mv.addObject("tecla1", tecla1);
+        mv.addObject("tecla2", tecla2); 
+        mv.addObject("tecla3", tecla3);
+        return mv;
+
+    }
+
+}
---
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/visortarea.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.securitasdirect.tareas.web.controller;
+
+import vistasController.pestañas.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ *
+ * @author jel
+ */
+public class visortarea implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
+        ModelAndView mv = new ModelAndView("visortarea");
+
+        String tecla1 = "active";
+        String tecla2 = "inactive";
+        String tecla3 = "inactive";
+        mv.addObject("tecla1", tecla1);
+        mv.addObject("tecla2", tecla2);
+        mv.addObject("tecla3", tecla3);
+
+        return mv;
+    }
+
+}
