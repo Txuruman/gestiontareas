0f6b3ff3fed4e91117fd98240cd4d0c67237526d
javiernaval <javiernaval@gmail.com>
2015-07-03 10:43:50 +0200

    varios

---
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/model/external/Pair.java
@@ -0,0 +1,35 @@
+package es.securitasdirect.tareas.model.external;
+
+/**
+ * Created by Javier Naval on 03/07/2015.
+ */
+public class Pair {
+
+    private Integer id;
+    private String value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Pair{");
+        sb.append("id=").append(id);
+        sb.append(", value='").append(value).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
