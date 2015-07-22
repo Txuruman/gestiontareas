505632bc13f8d462ebb2e3cace5ccbbed3d804f0
javiernaval <javiernaval@gmail.com>
2015-07-22 13:55:08 +0200

    delay

--- workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/maintenanceSurvey-ctrl.js
+++ workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/maintenanceSurvey-ctrl.js
@@ -1,5 +1,4 @@
 //Angular Maintenance Survey Controller start
-app.controller('maintenancesurvey-ctrl', function ($scope, $http, CommonService) {
 app.controller('maintenancesurvey-ctrl', function ($scope, $http, CommonService, $log) {
 
     $scope.getTarea = function () {
@@ -39,42 +38,69 @@ app.controller('maintenancesurvey-ctrl', function ($scope, $http, CommonService,
                 // or server returns response with an error status.
             });
         console.log("Loaded Excel Task Commons: Closing reason");
-    }
+    };
 
 
-    $scope.getInstallationAndTask = function(){
-    $scope.getInstallationAndTask = function($log){
-        $scope.vm.appReady=false;
+    $scope.getInstallationAndTask = function() {
+        $scope.getInstallationAndTask = function ($log) {
+            $scope.vm.appReady = false;
 
-        console.log("Loading MaintenanceTask...");
-        console.log("Loading Maintenance Survey Task...");
-        console.log("Params: "
-        + " installationId: " + $scope.installationId
-        + " ccUserId: " + $scope.ccUserId
-        + " callingList: " + $scope.callingList
-        + " taskId: " + $scope.tareaId);
-        $http({
-            method: 'GET',
-            url: 'maintenancesurveytask/getInstallationAndTask',
-            params: {installationId: $scope.installationId, ccUserId: $scope.ccUserId, callingList: $scope.callingList, tareaId: $scope.tareaId}
-        }).
-            success(function (data, status, headers, config) {
-                console.log("Loaded maintenance survey task:" + JSON.stringify(data.tarea));
-                $scope.tarea = data.tarea;
-                $scope.installationData = data.installationData;
-                CommonService.processBaseResponse(data,status,headers,config);
-                $scope.getClosingReason();
-                $scope.vm.appReady=true;
+            console.log("Loading MaintenanceTask...");
+            console.log("Loading Maintenance Survey Task...");
+            console.log("Params: "
+                + " installationId: " + $scope.installationId
+                + " ccUserId: " + $scope.ccUserId
+                + " callingList: " + $scope.callingList
+                + " taskId: " + $scope.tareaId);
+            $http({
+                method: 'GET',
+                url: 'maintenancesurveytask/getInstallationAndTask',
+                params: {installationId: $scope.installationId, ccUserId: $scope.ccUserId, callingList: $scope.callingList, tareaId: $scope.tareaId}
             }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                CommonService.processBaseResponse(data,status,headers,config);
-                $scope.vm.appReady=true;
-            });
-        console.log("MaintenanceTask loaded...")
-        console.log("Maintenance survey Task loaded...")
-    }
+                success(function (data, status, headers, config) {
+                    console.log("Loaded maintenance survey task:" + JSON.stringify(data.tarea));
+                    $scope.tarea = data.tarea;
+                    $scope.installationData = data.installationData;
+                    CommonService.processBaseResponse(data, status, headers, config);
+                    $scope.getClosingReason();
+                    $scope.vm.appReady = true;
+                }).
+                error(function (data, status, headers, config) {
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                    CommonService.processBaseResponse(data, status, headers, config);
+                    $scope.vm.appReady = true;
+                });
+            console.log("Maintenance survey Task loaded...")
+        }
+    };
+
+
+    //Ventana Aplazar - Start
+    //Abre la ventana, posibles tamaños '', 'sm', 'lg'
+    $scope.openDelayModal = function (size) {
+        var modalInstance = $modal.open({
+            animation: false, //Indica si animamos el modal
+            templateUrl: 'deplayModalContent.html', //HTML del modal
+            controller: 'DelayModalInstanceCtrl',  //Referencia al controller especifico para el modal
+            size: size,
+            resolve: {
+                //Creo que esto es para pasar parametros al controller interno
+                // items: function () {
+                //     return $scope.items;
+                // }
+            }
+        });
+
+        //Funciones para recivir el cierre ok y el cancel
+        modalInstance.result.then(function (delayInfo) {
+            //Boton Ok del modal
+            $scope.aplazar(delayInfo.delayDate, delayInfo.recallType);
+        }, function (param) {
+            //Boton cancelar del Modal
+        });
+    };
+    //Ventana Aplazar - End
 
 });
 //Angular Maintenance Survey Controller end
