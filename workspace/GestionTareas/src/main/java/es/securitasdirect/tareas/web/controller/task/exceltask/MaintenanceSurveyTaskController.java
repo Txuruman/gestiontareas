634badcd2b71edc501dee9776845892e08eb288e
agsgit <Pi8423121>
2015-08-18 14:06:36 +0200

    getInstallationAndTask

--- workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/task/exceltask/MaintenanceSurveyTaskController.java
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/task/exceltask/MaintenanceSurveyTaskController.java
@@ -4,6 +4,7 @@ import es.securitasdirect.tareas.model.InstallationData;
 import es.securitasdirect.tareas.model.tareaexcel.MaintenanceSurveyTask;
 import es.securitasdirect.tareas.service.InstallationService;
 import es.securitasdirect.tareas.service.QueryTareaService;
+import es.securitasdirect.tareas.web.controller.AgentController;
 import es.securitasdirect.tareas.web.controller.TaskController;
 import es.securitasdirect.tareas.web.controller.dto.TareaResponse;
 import es.securitasdirect.tareas.web.controller.dto.request.exceltask.maintenancesurvey.DiscardMaintenanceSurveyTaskRequest;
@@ -13,6 +14,7 @@ import es.securitasdirect.tareas.web.controller.dto.support.BaseResponse;
 import es.securitasdirect.tareas.web.controller.dto.support.DummyResponseGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -33,9 +35,13 @@ public class MaintenanceSurveyTaskController extends TaskController {
     private InstallationService installationDataService;
     @Inject
     private DummyResponseGenerator dummyResponseGenerator;
+    @Autowired
+    private AgentController agentController;
+
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MaintenanceSurveyTaskController.class);
 
+    /*
     @RequestMapping(value = "/gettarea", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody TareaResponse getMaintenanceSurveyTask(
         @RequestParam(value = "ccUserId", required = true) String ccUserId,
@@ -54,72 +60,52 @@ public class MaintenanceSurveyTaskController extends TaskController {
         }
         return response;
     }
+    */
 
 
     @RequestMapping(value = "/getInstallationAndTask", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody TareaResponse getInstallationAndTask(
-            @RequestParam(value = "installationId", required = true) String installationId, //TODO QUITAR ESTE PARAMETRO
-            @RequestParam(value = "ccUserId", required = true) String ccUserId,
             @RequestParam(value = "callingList", required = true) String callingList,
             @RequestParam(value = "tareaId", required = true) String tareaId
     ) {
 
-        LOGGER.debug("Get Notification task for params: \nccUserId:{}\ncallingList:{}\ntareaId:{}",ccUserId, callingList, tareaId);
+        LOGGER.debug("Get Notification task for params: \ncallingList:{}\ntareaId:{}", callingList, tareaId);
         TareaResponse response = new TareaResponse();
-        try {
-            //Buscar Tarea
-            MaintenanceSurveyTask task = (MaintenanceSurveyTask)queryTareaService.queryTarea(ccUserId, callingList, tareaId);
-            if (task!=null) {
-                response.setTarea(task);
-                //Buscamos la instalación
-                if (task.getNumeroInstalacion()!=null) {
-                    InstallationData installationData = installationDataService.getInstallationData(task.getNumeroInstalacion());
-                    if (installationData!=null) {
-                        response.setInstallationData(installationData);
+        if (agentController.isLogged()) {
+            try {
+                //Buscar Tarea
+                MaintenanceSurveyTask task = (MaintenanceSurveyTask)queryTareaService.queryTarea(
+                        agentController.getAgent().getIdAgent(),
+                        agentController.getAgent().getAgentCountryJob(),
+                        agentController.getAgent().getDesktopDepartment()
+                        , callingList, tareaId);
+                if (task!=null) {
+                    response.setTarea(task);
+                    //Buscamos la instalación
+                    if (task.getNumeroInstalacion()!=null) {
+                        InstallationData installationData = installationDataService.getInstallationData(task.getNumeroInstalacion());
+                        if (installationData!=null) {
+                            response.setInstallationData(installationData);
+                        } else {
+                            response.danger("getTask.noInstallation");
+                        }
                     } else {
                         response.danger("getTask.noInstallation");
                     }
                 } else {
-                    response.danger("getTask.noInstallation");
+                    response.danger("getTask.notFound");
                 }
-            } else {
-                response.danger("getTask.notFound");
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(),e);
+                processException(e);
             }
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(),e);
-            processException(e);
+        } else {
+            response.danger("agent.notLoggedIn");
         }
+
         return response;
 
 
-        /*
-        String TASK_SERVICE_MESSAGE = "maintenanceSurveyTask.getTask";
-        TareaResponse response = new TareaResponse();
-        LOGGER.debug("Get maintenance survey task for params: \nccUserId:{}\ncallingList:{}\ntareaId:{}", ccUserId, callingList, tareaId);
-        try{
-            MaintenanceSurveyTask tarea = (MaintenanceSurveyTask) queryTareaService.queryTarea(ccUserId, callingList, tareaId);
-            TareaResponse tareaResponse = processSuccessTask(tarea, TASK_SERVICE_MESSAGE);
-            response.addMessages(tareaResponse.getMessages());
-            response.setTarea(tareaResponse.getTarea());
-            LOGGER.debug("Maintenance survey task obtained from service: \n{}", tarea);
-        }catch(Exception e){
-            TareaResponse tareaResponse = processException(e, TASK_SERVICE_MESSAGE);
-            response.addMessages(tareaResponse.getMessages());
-        }
-        String INSTALLATION_SERVICE_MESSAGE = "installationData";
-        LOGGER.debug("Get installation data for params: \ninstallationId: {}", installationId);
-        try{
-            InstallationData installationData = installationDataService.getInstallationData(installationId);
-            TareaResponse installationResponse = processSuccessInstallation(installationData);
-            response.addMessages(installationResponse.getMessages());
-            response.setInstallationData(installationResponse.getInstallationData());
-            LOGGER.debug("Installation data obtained from service: \n{}", installationData);
-        }catch(Exception e){
-            TareaResponse installationResponse = processException(e, INSTALLATION_SERVICE_MESSAGE);
-            response.addMessages(installationResponse.getMessages());
-        }
-        return response;
-        */
     }
 
 
