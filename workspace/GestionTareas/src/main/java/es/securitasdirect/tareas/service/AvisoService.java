2bf02a8a717a97c86183254e8e50979774dee763
agsgit <Pi8423121>
2015-08-18 09:03:03 +0200

    query

--- workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/service/AvisoService.java
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/service/AvisoService.java
@@ -1,7 +1,10 @@
 package es.securitasdirect.tareas.service;
 
+import es.securitasdirect.tareas.model.tickets.*;
 import es.securitasdirect.tareas.model.tickets.operations.CreateTicket;
 import es.securitasdirect.tareas.support.XmlMarshaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wso2.ws.dataservice.SPAVISOSOPERACIONESPortType;
 import wsticketsv2.WsTickets;
 import wsticketsv2.WsTicketsSoap;
@@ -10,12 +13,18 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  *
  */
 @Named(value = "avisoService")
 @Singleton
 public class AvisoService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AvisoService.class);
 
     @Inject
     protected SPAVISOSOPERACIONESPortType spAvisosOperaciones;
@@ -24,13 +33,109 @@ public class AvisoService {
     @Inject
     protected XmlMarshaller xmlMarshaller;
 
-    public void createTicket(){
+    /**
+     * creacion del XML para crear un Aviso. Se hace a través de un WS disponible para la aplicación de Tickets.
+     */
+    public void createTicket(String idUser, String idCountry, String idLanguage){
+        /*
+         * Estructura del XML
+         */
         CreateTicket createTicket = new CreateTicket();
-        //TODO COPIAR TODO DEDE EL TEST DE XMLSERVICETEST Y MODIFICAR CON EL DISEÑO TECNICO
+
+        Req createReq = new Req();
+        Asgto createAsgto = new Asgto();
+        Comm createComm = new Comm();
+        Opcod createOpcod = new Opcod();
+        Clcod createClcod = new Clcod();
+        Item createItem = new Item();
+        List create_list_item = new ArrayList();
+
+        createTicket.setUser(new User());
+        createTicket.setTicket(new Ticket());
+        createTicket.setSvrq(new Svrq());
+
+
+        /* <REQ></REQ> */
+        createReq.setIdReq("ATC");
+        createReq.setReqName("");
+        createReq.setReqLname1("");
+        createReq.setReqLname2("");
+        createReq.setReqCif("");
+        createReq.setReqEmpl("I24311");
+        /* <ASGTO></ASGTO>*/
+        createAsgto.setIdAsg("");
+        createAsgto.setIdUser("");
+        /* <COMM></COMM>*/
+        createComm.setName("");
+        createComm.setlName1("");
+        createComm.setlName2("");
+        createComm.setInChannel("AUTO");
+        createComm.setValue("");
+        createComm.setComent("");
+        createComm.setOutChannel("");
+        createComm.setFrom("11");
+        createComm.setTo("18");
+        /* <OPCOD></OPCOD>*/
+        createOpcod.setCodKey1("200");
+        createOpcod.setCodKey2("210");
+        /* <CLCOD></CLCOD>*/
+        createClcod.setCodKey3("");
+        createClcod.setCodKey4("");
+
+        /* <ITEM></ITEM> */
+        createItem.setIdItemIBS("");
+        createItem.setCount("1");
+        createItem.setIdProblem("212");
+        createItem.setIdType("200");
+        create_list_item.add(createItem);
+
+        /*
+         * <USER></USER>
+         */
+        createTicket.getUser().setIdUser("I24311");
+        createTicket.getUser().setIdUsidCountry("1");
+        createTicket.getUser().setIdLanguage("ES");
+        createTicket.getUser().setT("NOSESSION");
+
+        /*
+         *
+         * <TICKET></TICKET>
+         */
+        createTicket.getTicket().setNumInst("1606430");
+        createTicket.getTicket().setObserv("Texto Aviso");
+        createTicket.getTicket().setCodZIP("28030");
+        createTicket.getTicket().setCloseTicket("0");
+        createTicket.getTicket().setDataAditional("");
+        createTicket.getTicket().setNoteClose("");
+        createTicket.getTicket().setMorDebt("0");
+        createTicket.getTicket().setTypePanel("SDVFAST");
+
+        /* <REQ></REQ>
+         */
+        createTicket.getTicket().setReq(createReq);
+        createTicket.getTicket().setAsgto(createAsgto);
+        createTicket.getTicket().setComm(createComm);
+        createTicket.getTicket().setOpcod(createOpcod);
+        createTicket.getTicket().setClcod(createClcod);
+
+        /*
+         * <SVRQ></SVRQ>
+         */
+        createTicket.getSvrq().setMakeSVRQ("0");
+        createTicket.getSvrq().setIdTec("");
+        createTicket.getSvrq().setInsBoli("0");
+        createTicket.getSvrq().setItems(new ArrayList<Item>());
+        createTicket.getSvrq().setItems(create_list_item);
+
+
+
 
         String xmlCreateTicket = xmlMarshaller.marshalObject(createTicket);
 
-        wsTickets.create(xmlCreateTicket);
+
+        String xmlResult = wsTickets.create(xmlCreateTicket);
+
+        LOGGER.debug("xmlCreateTicket: {} xmlResult:{}", xmlCreateTicket,xmlResult);
     }
 
 
