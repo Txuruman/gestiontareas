1e6e84eb744904ab907f9f75b0b01b039c93e87a
agsgit <Pi8423121>
2015-08-18 12:51:01 +0200

    getInstallationAndTask

--- workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/task/exceltask/ListAssitantTaskController.java
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/web/controller/task/exceltask/ListAssitantTaskController.java
@@ -84,11 +84,41 @@ public class ListAssitantTaskController extends TaskController {
 
     @RequestMapping(value = "/getInstallationAndTask", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public @ResponseBody TareaResponse getInstallationAndTask(
-            @RequestParam(value = "installationId", required = true) String installationId,
+            @RequestParam(value = "installationId", required = true) String installationId, //TODO QUITAR ESTE PARAMETRO
             @RequestParam(value = "ccUserId", required = true) String ccUserId,
             @RequestParam(value = "callingList", required = true) String callingList,
             @RequestParam(value = "tareaId", required = true) String tareaId
     ) {
+
+        LOGGER.debug("Get Notification task for params: \nccUserId:{}\ncallingList:{}\ntareaId:{}",ccUserId, callingList, tareaId);
+        TareaResponse response = new TareaResponse();
+        try {
+            //Buscar Tarea
+            TareaListadoAssistant task = (TareaListadoAssistant)queryTareaService.queryTarea(ccUserId, callingList, tareaId);
+            if (task!=null) {
+                response.setTarea(task);
+                //Buscamos la instalación
+                if (task.getNumeroInstalacion()!=null) {
+                    InstallationData installationData = installationDataService.getInstallationData(task.getNumeroInstalacion());
+                    if (installationData!=null) {
+                        response.setInstallationData(installationData);
+                    } else {
+                        response.danger("getTask.noInstallation");
+                    }
+                } else {
+                    response.danger("getTask.noInstallation");
+                }
+            } else {
+                response.danger("getTask.notFound");
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(),e);
+            processException(e);
+        }
+        return response;
+
+
+        /*
         TareaResponse response = new TareaResponse();
         String TASK_SERVICE_MESSAGE = "listAssistantTask.getTask";
         LOGGER.debug("Get maintenance task for params: \nccUserId:{}\ncallingList:{}\ntareaId:{}", ccUserId, callingList, tareaId);
@@ -115,5 +145,6 @@ public class ListAssitantTaskController extends TaskController {
             response.addMessages(tareaResponse.getMessages());
         }
         return response;
+        */
     }
 }
