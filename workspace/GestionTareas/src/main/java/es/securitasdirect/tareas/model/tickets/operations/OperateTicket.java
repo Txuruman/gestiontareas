f46ca6183429382d652e53335a5a7de890096f7e
agsgit <Pi8423121>
2015-08-24 09:09:40 +0200

    se nombra como OperateTicket

--- workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/model/tickets/operations/OperateTicket.java
+++ workspace/GestionTareas/src/main/java/es/securitasdirect/tareas/model/tickets/operations/OperateTicket.java
@@ -16,7 +16,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement(name = "DATA")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class CreateTicket {
+public class OperateTicket {
 
 
     /*
