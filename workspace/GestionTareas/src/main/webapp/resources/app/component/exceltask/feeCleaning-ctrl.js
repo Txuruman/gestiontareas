312902142621fdf8f61c3a71e92495b1c60ce59f
agsgit <Pi8423121>
2015-09-01 13:50:18 +0200

    bp_desktopDepartment

--- workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/feeCleaning-ctrl.js
+++ workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/feeCleaning-ctrl.js
@@ -118,7 +118,7 @@ app.controller('feecleaningtask-ctrl', function ($scope, $http, CommonService, $
      * Variable _contextPath inicializada en commonImports
      */
     $scope.descartar=function(){
-    	$window.location.href= _contextPath + "/entry?bp_agent=12187&bp_agentIBS=M0OOS&bp_agentCountryJob=SPAIN&bp_desktopDepartment=ATC_SPN";
+    	$window.location.href= _contextPath + "/entry?bp_agent=12187&bp_agentIBS=M0OOS&bp_agentCountryJob=SPAIN&bp_desktopDepartment=ATC";
     }    
     
 //Antiguo método descartar
