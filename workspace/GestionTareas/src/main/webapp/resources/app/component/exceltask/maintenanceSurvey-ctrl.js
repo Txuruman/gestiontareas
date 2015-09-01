0ba5a8c59c07d708d6b1eb8219556d3e8f0ec6dc
agsgit <Pi8423121>
2015-09-01 13:50:59 +0200

    bp_desktopDepartment

--- workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/maintenanceSurvey-ctrl.js
+++ workspace/GestionTareas/src/main/webapp/resources/app/component/exceltask/maintenanceSurvey-ctrl.js
@@ -106,7 +106,7 @@ app.controller('maintenancesurvey-ctrl', function ($scope, $http, CommonService,
      * Variable _contextPath inicializada en commonImports
      */
     $scope.descartar=function(){
-    	$window.location.href= _contextPath + "/entry?bp_agent=12187&bp_agentIBS=M0OOS&bp_agentCountryJob=SPAIN&bp_desktopDepartment=ATC_SPN";
+    	$window.location.href= _contextPath + "/entry?bp_agent=12187&bp_agentIBS=M0OOS&bp_agentCountryJob=SPAIN&bp_desktopDepartment=ATC";
     }    
     
 //Antiguo método descartar    
